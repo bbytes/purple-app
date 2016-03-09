0f1406df9fe9dbb93863e96e7c8e294af77d8f9b
Akshay <akshay.nag@beyondbytes.co.in>
2016-03-09 17:34:16 +0530

    Not use actually

--- purple-server/src/main/java/com/bbytes/purple/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bbytes.purple.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.bbytes.purple.domain.Permission;
-
-public interface PermissionRepository extends MongoRepository<Permission, String>{
-
-}
--- purple-server/src/main/java/com/bbytes/purple/repository/ReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bbytes.purple.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.bbytes.purple.domain.Reply;
-
-public interface ReplyRepository extends MongoRepository<Reply, String>{
-
-}
