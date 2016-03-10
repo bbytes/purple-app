d11744e89979ff007fba7ac611f60ebece3d4da4
nkamath <n.kamath@beyondbytes.co.in>
2016-03-10 15:10:45 +0530

    PURPLE-7 : Create message transfer format

---
+++ purple-server/src/main/java/com/bbytes/purple/rest/dto/models/RestResponse.java
@@ -0,0 +1,68 @@
+package com.bbytes.purple.rest.dto.models;
+
+import java.io.Serializable;
+
+public class RestResponse implements Serializable {
+	
+	private static final long serialVersionUID = 7996664536603472824L;
+
+	public static final String TOTAL_SIZE = "tsize";
+	public static final String GRID_DATA = "gridData";
+	
+	private boolean success = false;
+	
+	private Object data;
+	
+	private String reason = null;
+
+	/**
+	 * Success response
+	 * @param success
+	 * @param data
+	 */
+	public RestResponse(boolean success, Object data) {
+		this.success = success;
+		this.data = data;
+	}
+	
+	/**
+	 * Failure response
+	 * @param data
+	 * @param reason
+	 */
+	public RestResponse(String data, String reason) {
+		this.data = data;
+		this.reason = reason;
+	}
+	
+	public RestResponse(boolean success, Object data, String reason) {
+		this.success = success;
+		this.data = data;
+		this.reason = reason;
+	}
+	
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+}
