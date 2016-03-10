1b2d5d3ed69fa373c580c55bd0f0783bf1aba5b9
jaya <jayashree@beyondbytes.co.in>
2016-03-10 18:04:13 +0530

    added login controller

--- purple-ui/source/app/controllers/loginCtrl.js
+++ purple-ui/source/app/controllers/loginCtrl.js
@@ -1,15 +1,47 @@
-rootApp.controller('loginCtrl', function ($scope, $rootScope, $state, loginService, appNotifyService) {
+rootApp.controller('loginCtrl', function ($scope, $rootScope, $state, loginService) {
 
     $scope.submitLoginForm = function (isValid) {
-
+   console.log("message");
         // Validating login form
         if (!isValid) {
-            appNotifyService.error('Please enter username and password', 'Invalid inputs');
+           console.log('Please enter username and password', 'Invalid inputs');
             return false;
         }
 
         // Calling login service
-        loginService.login($scope.usrName, $scope.usrPass).then(function (response) {
+        loginService.login($scope.username, $scope.password).then(function (response) {
+         if (response.headers["x-auth-token"]) {
+                
+                $rootScope.loggedStatus = true;
+                $rootScope.loggedIn = $scope.username;
+                $rootScope.userName = response.data.name;
+                $rootScope.authToken = response.headers["x-auth-token"];
+                $rootScope.permissions = response.data.permissions;
+
+                var userInfo = {
+                    accessToken: response.headers["x-auth-token"],
+                    id: $rootScope.loggedIn,
+                    name: $rootScope.userName,
+                    userRoles: response.data.userRoles,
+                    permissions: response.data.permissions,
+                    viewMode:$rootScope.viewMode
+                };
+                
+                $sessionStorage.userInfo = userInfo;
+
+                // Login successful, set user locale and Redirect to home page
+                if(response.data.locale){
+                    appLocaleService.setLocale(response.data.locale);
+                }
+
+                $rootScope.showWelcomeMessage = true;
+                
+                $state.go('home');
+                
+            } else {
+                //Login failed. Showing error notification
+                appNotifyService.error(response.data, 'Login Failed.');
+            }
 
         }, function (error) {
             //Login failed. Showing error notification
