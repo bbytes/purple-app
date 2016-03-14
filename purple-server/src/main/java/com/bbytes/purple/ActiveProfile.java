c429d564f1f4ccd5ee3ca7ce5eceeef1da7f205f
tanmally <tm@beyondbytes.co.in>
2016-03-14 23:13:30 +0530

    Added code to print  the active profile

---
+++ purple-server/src/main/java/com/bbytes/purple/ActiveProfile.java
@@ -0,0 +1,20 @@
+package com.bbytes.purple;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveProfile implements EnvironmentAware {
+	private static final Logger logger = LoggerFactory.getLogger(ActiveProfile.class);
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		logger.info("###### Active Profiles ######");
+		logger.info( " " + StringUtils.join(environment.getActiveProfiles(),",") + " ");
+		logger.info("#############################");
+	}
+}
