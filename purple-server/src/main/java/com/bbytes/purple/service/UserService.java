7fc73da7ed6d68eb4ebc1c0a20c3513675ad6a7a
Akshay <akshay.nag@beyondbytes.co.in>
2016-03-16 18:06:41 +0530

    Set user role for very first time sign up

--- purple-server/src/main/java/com/bbytes/purple/service/UserService.java
+++ purple-server/src/main/java/com/bbytes/purple/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.bbytes.purple.domain.Organization;
 import com.bbytes.purple.domain.User;
+import com.bbytes.purple.domain.UserRole;
 import com.bbytes.purple.repository.UserRepository;
 
 @Service
@@ -57,6 +58,7 @@ public class UserService extends AbstractService<User, String> {
 		 User user = new User(name, email);
 		 user.setOrganization(org);
 		 user.setPassword(passwordHashService.encodePassword(password));
+		 user.setUserRole(UserRole.ADMIN_USER_ROLE);
 		 return userRepository.save(user);
 	}
 
