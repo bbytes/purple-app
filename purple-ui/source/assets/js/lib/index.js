ea9c14a5a399908af41a456449013c4277cd0583
sanjay-bbytes <sanjaykumar@beyondbytes.co.in>
2016-03-20 17:07:13 +0530

    Adding all updated UI templates

--- purple-ui/source/assets/js/lib/index.js
+++ purple-ui/source/assets/js/lib/index.js
@@ -14,8 +14,7 @@
             size  = size || 60;
 
             var colours = [
-                    "#222", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", 
-                    "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5", "#cc95b5"
+                    "#1abc9c", "#16a085", "#f1c40f", "#f39c12", "#2ecc71", "#27ae60", "#e67e22", "#d35400", "#3498db", "#2980b9", "#e74c3c", "#c0392b", "#9b59b6", "#8e44ad", "#bdc3c7", "#34495e", "#2c3e50", "#95a5a6", "#7f8c8d", "#ec87bf", "#d870ad", "#f69785", "#9ba37e", "#b49255", "#b49255", "#a94136"
                 ],
 
                 nameSplit = String(name).toUpperCase().split(' '),
