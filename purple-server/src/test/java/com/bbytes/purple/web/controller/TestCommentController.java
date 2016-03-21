069f6a85efab9e9bb20de9a946c7ea96dc0f8912
Aditya <aditya@beyondbytes.co.in>
2016-03-21 16:49:19 +0530

    Reverting the Comment and Reply controller

--- purple-server/src/test/java/com/bbytes/purple/web/controller/TestCommentController.java
+++ purple-server/src/test/java/com/bbytes/purple/web/controller/TestCommentController.java
@@ -29,50 +29,5 @@ import com.fasterxml.jackson.databind.SerializationFeature;
  */
 public class TestCommentController extends PurpleWebBaseApplicationTests {
 
-	Organization org;
-	User user;
-	Comment comment;
-	Status status;
-
-	@Before
-	public void setUp() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(context).dispatchOptions(true).addFilters(filterChainProxy)
-				.build();
-
-		org = new Organization("abc", "abc@org");
-		organizationService.save(org);
-
-		TenancyContextHolder.setTenant(org.getOrgId());
-	}
-
-	@Test
-	public void addCommentTest() throws Exception {
-		mockMvc.perform(post("/api/v1/comment/add")).andExpect(status().is4xxClientError()).andDo(print());
-
-	}
-
-	@Test
-	public void saveComment() throws Exception {
-		user = new User("user1", "user1@abc.org");
-		user.setOrganization(org);
-		userService.save(user);
-
-		CommentDTO requestCommentDTO = new CommentDTO();
-		requestCommentDTO.setComment("YO YO PURPLE");
-		requestCommentDTO.setStatus(new Status("Purple", "Revil", 8, new DateTime()));
-		requestCommentDTO.setUser(user);
-
-		organizationService.save(org);
-
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-		String requestJson = ow.writeValueAsString(requestCommentDTO);
-
-		String xauthToken = tokenAuthenticationProvider.getAuthTokenForUser(requestCommentDTO.getUser().getEmail(), 1);
-		mockMvc.perform(post("/api/v1/comment/add").header(GlobalConstants.HEADER_AUTH_TOKEN, xauthToken)
-				.contentType(APPLICATION_JSON_UTF8).content(requestJson)).andExpect(status().is5xxServerError())
-				.andDo(print()).andExpect(content().string(containsString("{\"success\":false")));
-	}
-
+	
 }
