6890084efdc8ebd4420522c5ac33916232a0d02b
Akshay <akshay.nag@beyondbytes.co.in>
2016-03-21 16:28:15 +0530

    Clean up

--- purple-server/src/main/java/com/bbytes/purple/utils/SuccessHandler.java
+++ purple-server/src/main/java/com/bbytes/purple/utils/SuccessHandler.java
@@ -17,7 +17,6 @@ public class SuccessHandler {
 	public static final String DELETE_PROJECT_SUCCESS = "delete_project_success";
 
 	public static final String GET_PROJECT_SUCCESS = "get_projects_success";
-	
 
 	public static final String GET_REPLY_SUCCESS = "get_reply_success";
 	
