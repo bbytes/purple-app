5396f512cbb5bfb7172299d2f23856a2ec9158ac
jaya <jayashree@beyondbytes.co.in>
2016-03-24 00:27:34 +0530

    create ,delete and display projects fixed

---
+++ purple-ui/source/app/controllers/allusersModalCtrl.js
@@ -0,0 +1,51 @@
+rootApp.controller('allusersModalCtrl', function ($scope,$rootScope,adminService,options,$uibModalInstance,$uibModal) {
+
+	$scope.selection =[];
+   $scope.title = options.title;
+    
+    $scope.allusers = options.data;
+    console.log( $scope.allusers);
+    
+    $scope.initUser = function() {
+    adminService.getAllusers().then(function (response) {
+        if (response.success) {
+        	if (response) {
+				$scope.userscount = response.data.length;
+			}
+            $scope.allusers   =response.data.gridData;;
+        }
+    });
+    }
+    
+    
+    $scope.toggleSelection = function toggleSelection(id) {
+        var idx = $scope.selection.indexOf(id);
+
+        // is currently selected
+        if (idx > -1) {
+            $scope.selection.splice(idx, 1);
+          }
+
+          // is newly selected
+          else {
+            $scope.selection.push(id);
+          }
+       
+        // is newly selected
+         
+         
+        
+      };
+   $scope.ok = function(){
+	    console.log($scope.selection);
+	
+	  
+    	$uibModalInstance.close($scope.selection);
+    	
+    	
+    }
+    
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
---
+++ purple-ui/source/app/services/projectService.js
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+
+rootApp.service('projectService', function($rootScope, $http, $q, $window) {
+
+	this.createProject = function(project) {
+
+		var deferred = $q.defer();
+
+		$http({
+			method : 'POST',
+			url : $rootScope.baseUrl + '/api/v1/admin/project/create',
+			data : project,
+			headers : {
+				'Content-Type' : 'application/json',
+
+			}
+
+		}).success(function(response, status, headers, config) {
+
+			deferred.resolve(response);
+		}).error(function() {
+			// Something went wrong.
+			deferred.reject({
+				'success' : false,
+				'msg' : 'Oops! Something went wrong. Please try again later.'
+			});
+		});
+
+		return deferred.promise;
+
+	};
+
+	this.getAllprojects = function() {
+
+		var deferred = $q.defer();
+
+		$http({
+			method : 'GET',
+			url : $rootScope.baseUrl + '/api/v1/admin/project',
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).success(function(response, status, headers, config) {
+			deferred.resolve(response);
+		}).error(function() {
+			deferred.reject({
+				'success' : false,
+				'msg' : 'Oops! Something went wrong. Please try again later.'
+			});
+		});
+
+		return deferred.promise;
+	};
+	
+	this.deleteProject = function(id) {
+
+		var deferred = $q.defer();
+
+		$http({
+			method :'DELETE',
+			url : $rootScope.baseUrl + '/api/v1/admin/project/delete/' +id,
+			//data : admin,
+			headers : {
+				'Content-Type' : 'application/json',
+
+			}
+
+		}).success(function(response, status, headers, config) {
+
+			deferred.resolve(response);
+		}).error(function() {
+			// Something went wrong.
+			deferred.reject({
+				'success' : false,
+				'msg' : 'Oops! Something went wrong. Please try again later.'
+			});
+		});
+
+		return deferred.promise;
+
+	};
+
+});
