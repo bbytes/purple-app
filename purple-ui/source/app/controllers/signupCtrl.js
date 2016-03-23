9bd725724dc35a581a5787d0d61539a7631a9ff3
jaya <jayashree@beyondbytes.co.in>
2016-03-24 00:30:51 +0530

    Purple 25: all functionalities in user-manager page invite,view users ,count and some bug fixes

--- purple-ui/source/app/controllers/signupCtrl.js
+++ purple-ui/source/app/controllers/signupCtrl.js
@@ -1,4 +1,4 @@
-rootApp.controller('signupCtrl', function ($scope, $rootScope, $state, signupService,appNotifyService) {
+rootApp.controller('signupCtrl', function ($scope, $rootScope, $state, signupService,appNotifyService,$sessionStorage) {
   
     $rootScope.bodyClass = 'body-standalone';
     
@@ -16,8 +16,18 @@ rootApp.controller('signupCtrl', function ($scope, $rootScope, $state, signupSer
         signupService.submitSignUp($scope.user).then(function (response) {
          if (response.success == true) {
                 
-                
-                $state.go('user-mgr');
+        	/*   $rootScope.authToken = response.data;
+        	   var userInfo = {
+                       authToken: response.data,
+                       id: $rootScope.loggedIn,
+                       //name: $rootScope.userName,
+                      // userRoles: response.data.userRoles,
+                      // permissions: response.data.permissions,
+                      // viewMode:$rootScope.viewMode
+                   };
+                   
+                 $sessionStorage.userInfo = userInfo;*/
+                $state.go('login');
                 
             } else {
                 //Login failed. Showing error notification
