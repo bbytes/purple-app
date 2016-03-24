de32dd3ba523faaf849b8d7cf07ab57b0072d60e
tanmally <tm@beyondbytes.co.in>
2016-03-25 03:16:54 +0530

    PURPLE-34 : Fix for DB connection inconsistent and DB getting deleted.The embedded mode was kicking off in dev and prod mode dues ot auto config of boot . Excluded embedded mongo auto config

--- purple-server/src/main/java/com/bbytes/purple/PurpleApplication.java
+++ purple-server/src/main/java/com/bbytes/purple/PurpleApplication.java
@@ -1,8 +1,11 @@
 package com.bbytes.purple;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={EmbeddedMongoAutoConfiguration.class})
 @SpringBootApplication
 public class PurpleApplication {
 
--- purple-server/src/test/java/com/bbytes/purple/PurpleApplicationTests.java
+++ purple-server/src/test/java/com/bbytes/purple/PurpleApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
