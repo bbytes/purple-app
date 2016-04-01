7b72a406ece31287059797442b4649eb5ab75919
sanjay-bbytes <sanjaykumar@beyondbytes.co.in>
2016-04-01 18:54:42 +0530

    added comment service js

---
+++ purple-ui/source/app/services/commentService.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+rootApp.service('commentService', function($rootScope, $http, $q, $window) {
+
+	this.submitStatus = function(status) {
+
+		var deferred = $q.defer();
+
+		$http({
+			method : 'POST',
+			url : $rootScope.baseUrl + '/api/v1/comment/add',
+			data : status,
+			headers : {
+				'Content-Type' : 'application/json',
+
+			}
+
+		}).success(function(response, status, headers, config) {
+
+			deferred.resolve(response);
+		}).error(function() {
+			// Something went wrong.
+			deferred.reject({
+				'success' : false,
+				'msg' : 'Oops! Something went wrong. Please try again later.'
+			});
+		});
+
+		return deferred.promise;
+
+	};
+	
+	});
