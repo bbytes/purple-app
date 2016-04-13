87dbad1b2b5dfa41aec6ae3bad9b376937fea718
tanmally <tm@beyondbytes.co.in>
2016-04-14 04:42:37 +0530

    PURPLE-64 : We need not turn off html5 mode , the issue is fixed with redirect of angular js urls

--- purple-server/src/main/java/com/bbytes/purple/auth/jwt/SpringSecurityConfig.java
+++ purple-server/src/main/java/com/bbytes/purple/auth/jwt/SpringSecurityConfig.java
@@ -44,8 +44,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity webSecurity) throws Exception {
 		webSecurity.ignoring()
-				// All of Spring Security will ignore the requests
-				.antMatchers("/").antMatchers("/web/**").antMatchers("/index.html").antMatchers("/resources/**")
+				// All of Spring Security will ignore the requests. '/{[path:[^\\.]*}' is to avoid all the angualr internal urls 
+				.antMatchers("/").antMatchers("/{[path:[^\\.]*}").antMatchers("/resources/**")
 				.antMatchers("/assets/**").antMatchers("/favicon.ico").antMatchers("/**/*.html")
 				.antMatchers("/resources/**").antMatchers("/**/*.css").antMatchers("/**/*.js");
 	}
@@ -64,16 +64,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.exceptionHandling().and().servletApi().and().authorizeRequests()
 
-				// // Allow anonymous resource requests
-				// .antMatchers("/").permitAll().antMatchers("/index.html").permitAll().antMatchers("/favicon.ico")
-				// .permitAll().antMatchers("/**/*.html").permitAll().antMatchers("/**/*.css").permitAll()
-				// .antMatchers("/**/*.js").permitAll()
-
 				// Allow logins urls
 				.antMatchers("/auth/**").permitAll()
-
-				// All other request need to be authenticated
-				.anyRequest().authenticated().and()
+				.antMatchers("/api/**").authenticated().and()
 
 				// Custom Token based authentication based on the header
 				// previously given to the client
--- purple-server/src/main/java/com/bbytes/purple/web/controller/HTML5AngularRedirectController.java
+++ purple-server/src/main/java/com/bbytes/purple/web/controller/HTML5AngularRedirectController.java
@@ -1,14 +1,13 @@
 package com.bbytes.purple.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HTML5AngularRedirectController {
 
-	@RequestMapping("/web/{page}/**")
-	public String app(@PathVariable String page) {
-		return "redirect:/#" + page;
+	@RequestMapping(value = "/{[path:[^\\.]*}")
+	public String redirect() {
+		return "forward:/";
 	}
 }
