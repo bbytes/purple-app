2428375429b7cd2c23d9e85de81c647ffb7b04da
Akshay <akshay.nag@beyondbytes.co.in>
2016-04-18 15:03:02 +0530

    Purple 65 & 70 - Added proper growl

--- purple-ui/source/app/controllers/resetPasswordCtrl.js
+++ purple-ui/source/app/controllers/resetPasswordCtrl.js
@@ -9,25 +9,22 @@ rootApp.controller('resetPasswordCtrl', function ($scope, $rootScope, $state, $w
          
          var tokenstored = $rootScope.authToken;
 
-    $scope.updatePassword = function (isValid) {
-    	   if (!isValid) {
-    	        appNotifyService.error('Please enter valid passwords', 'Invalid inputs');
-    	           console.log('Please enter username and password', 'Invalid inputs');
+    $scope.updatePassword = function (user) {
+    	   if (user.newPassword != user.confirmPassword) {
+    	        appNotifyService.error('Password is mismatch');
     	            return false;
     	        }
       
     	settingsService.updatePassword($scope.user).then(function (response) {
         	 if (response.success = true) {
-                console.log(response);
         		 $scope.user ='';
-        		 appNotifyService.success('yes', 'Your password has been changed.');  		 
+        		 appNotifyService.success('Your password has been reset'); 
+                   $state.go('login');  		 
             } else {
 
                 appNotifyService.error(response.data, 'Enter valid passwords.');
             }
-
         }, function (error) {
-
             appNotifyService.error(error.msg, 'Enter valid passwords.');
         });
     };
