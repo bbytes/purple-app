cacd486455121d10004d8809509c8844f5eeb024
tanmally <tm@beyondbytes.co.in>
2016-05-08 14:30:51 +0530

    Fixed mongo username password issue for test case

--- purple-server/src/test/java/com/bbytes/purple/database/TestMongoDatabaseConfig.java
+++ purple-server/src/test/java/com/bbytes/purple/database/TestMongoDatabaseConfig.java
@@ -13,6 +13,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 
 import de.flapdoodle.embed.mongo.MongodExecutable;
@@ -28,18 +29,18 @@ import de.flapdoodle.embed.mongo.distribution.Version;
 @EnableMongoAuditing
 @Profile("test")
 public class TestMongoDatabaseConfig {
-	
-    private static final MongodStarter starter = MongodStarter.getDefaultInstance();
 
-    @Autowired
-    private MongoProperties properties;
-    
-    @Autowired
-    private Environment env;
+	private static final MongodStarter starter = MongodStarter.getDefaultInstance();
 
-    @Autowired(required = false)
-    private MongoClientOptions options;
-    
+	@Autowired
+	private MongoProperties properties;
+
+	@Autowired
+	private Environment env;
+
+	@Autowired(required = false)
+	private MongoClientOptions options;
+
 	@Value("${spring.data.mongodb.host}")
 	private String host;
 
@@ -48,35 +49,38 @@ public class TestMongoDatabaseConfig {
 
 	@Value("${spring.data.mongodb.database}")
 	private String database;
-	
-//	@Value("${spring.data.mongodb.username}")
-//	private String username;
-//
-//	@Value("${spring.data.mongodb.password}")
-//	private String password;
 
+	@Value("${spring.data.mongodb.username}")
+	private String username;
+
+	@Value("${spring.data.mongodb.password}")
+	private String password;
+
+	@Value("${spring.data.mongodb.auth.database}")
+	private String authDatabase;
 
 	@Bean(destroyMethod = "close")
-    public Mongo mongo() throws IOException {
-        Net net = mongod().getConfig().net();
-        properties.setHost(net.getServerAddress().getHostName());
-        properties.setPort(net.getPort());
-        return properties.createMongoClient(this.options,env);
-    }
+	public Mongo mongo() throws IOException {
+		Net net = mongod().getConfig().net();
+		properties.setHost(net.getServerAddress().getHostName());
+		properties.setPort(net.getPort());
+		// return properties.createMongoClient(this.options,env);
+		return new MongoClient(net.getServerAddress().getHostName());
+	}
 
-    @Bean(destroyMethod = "stop")
-    public MongodProcess mongod() throws IOException {
-        return mongodExe().start();
-    }
+	@Bean(destroyMethod = "stop")
+	public MongodProcess mongod() throws IOException {
+		return mongodExe().start();
+	}
 
-    @Bean(destroyMethod = "stop")
-    public MongodExecutable mongodExe() throws IOException {
-        return starter.prepare(mongodConfig());
-    }
+	@Bean(destroyMethod = "stop")
+	public MongodExecutable mongodExe() throws IOException {
+		return starter.prepare(mongodConfig());
+	}
 
-    @Bean
-    public IMongodConfig mongodConfig() throws IOException {
-        return new MongodConfigBuilder().version(Version.Main.PRODUCTION).build();
-    }
+	@Bean
+	public IMongodConfig mongodConfig() throws IOException {
+		return new MongodConfigBuilder().version(Version.Main.PRODUCTION).build();
+	}
 
 }
