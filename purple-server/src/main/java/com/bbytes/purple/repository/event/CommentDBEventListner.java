4c1d8892d3131956326cf8cb70a8b40f102854d7
Akshay <akshay.nag@beyondbytes.co.in>
2016-05-17 17:55:16 +0530

    Purple 110 - Added email service post comment

--- purple-server/src/main/java/com/bbytes/purple/repository/event/CommentDBEventListner.java
+++ purple-server/src/main/java/com/bbytes/purple/repository/event/CommentDBEventListner.java
@@ -1,10 +1,5 @@
 package com.bbytes.purple.repository.event;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -15,10 +10,7 @@ import org.springframework.stereotype.Component;
 import com.bbytes.purple.domain.Comment;
 import com.bbytes.purple.domain.Status;
 import com.bbytes.purple.service.CommentService;
-import com.bbytes.purple.service.EmailService;
 import com.bbytes.purple.service.StatusService;
-import com.bbytes.purple.service.UserService;
-import com.bbytes.purple.utils.GlobalConstants;
 
 @Component
 @Profile("saas")
@@ -31,13 +23,7 @@ public class CommentDBEventListner extends AbstractMongoEventListener<Comment> {
 	private CommentService commentService;
 
 	@Autowired
-	private UserService userService;
-
-	@Autowired
 	MongoTemplate mongoTemplate;
-
-	@Autowired
-	private EmailService emailService;
 
 	/**
 	 * Update commentCount in status object after saving comment.
@@ -45,26 +31,11 @@ public class CommentDBEventListner extends AbstractMongoEventListener<Comment> {
 	@Override
 	public void onAfterSave(AfterSaveEvent<Comment> event) {
 
-		final String subject = GlobalConstants.EMAIL_STATUS_COMMENT_SUBJECT;
-		final String template = GlobalConstants.COMMENT_EMAIL_TEMPLATE;
-
 		Comment commentSaved = event.getSource();
 		Status status = statusService.findOne(commentSaved.getStatus().getStatusId());
 		long count = commentService.getCountByStatus(status);
 		status.setCommentCount(count);
 		statusService.save(status);
 
-		String userName = userService.getLoggedinUser().getName();
-
-		List<String> emailList = new ArrayList<String>();
-		emailList.add(status.getUser().getEmail());
-
-		Map<String, Object> emailBody = new HashMap<>();
-		emailBody.put(GlobalConstants.USER_NAME, status.getUser().getName());
-		emailBody.put(GlobalConstants.SUBSCRIPTION_DATE, status.getDateTime());
-		emailBody.put("userName", userName);
-
-		emailService.sendEmail(emailList, emailBody, subject, template);
 	}
-
 }
