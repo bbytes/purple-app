aa6796165fd97af8432c6248933be4d04a69955d
Akshay <akshay.nag@beyondbytes.co.in>
2016-05-20 02:50:33 +0530

    Added event for delete comment and update commentcount

--- purple-server/src/main/java/com/bbytes/purple/repository/event/CommentDBEventListner.java
+++ purple-server/src/main/java/com/bbytes/purple/repository/event/CommentDBEventListner.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
+import org.springframework.data.mongodb.core.mapping.event.BeforeDeleteEvent;
 import org.springframework.stereotype.Component;
 
 import com.bbytes.purple.domain.Comment;
 import com.bbytes.purple.domain.Status;
 import com.bbytes.purple.service.CommentService;
 import com.bbytes.purple.service.StatusService;
+import com.mongodb.DBObject;
 
 @Component
 @Profile("saas")
@@ -24,6 +26,22 @@ public class CommentDBEventListner extends AbstractMongoEventListener<Comment> {
 
 	@Autowired
 	MongoTemplate mongoTemplate;
+	
+	/**
+	 * After removing comment, update commentCount in status object.
+	 */
+	
+	@Override
+	public void onBeforeDelete(BeforeDeleteEvent<Comment> event) {
+		final DBObject commentDeleted = event.getSource();
+		Comment comment = commentService.findOne(commentDeleted.get("commentId").toString());
+		Status status = statusService.findOne(comment.getStatus().getStatusId());
+		long count = commentService.getCountByStatus(status);
+		count--;
+		status.setCommentCount(count);
+		statusService.save(status);
+
+	}
 
 	/**
 	 * Update commentCount in status object after saving comment.
@@ -38,4 +56,6 @@ public class CommentDBEventListner extends AbstractMongoEventListener<Comment> {
 		statusService.save(status);
 
 	}
+
+	
 }
