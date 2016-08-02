41326e7bcd1a25dc40fbe83540c7d53777f15790
Akshay <akshay.nag@beyondbytes.co.in>
2016-08-02 18:41:25 +0530

    changed in logout

--- purple-ui/source/app/controllers/logoutCtrl.js
+++ purple-ui/source/app/controllers/logoutCtrl.js
@@ -6,8 +6,9 @@ rootApp.controller('logoutCtrl', function ($scope, $rootScope, $state, logoutSer
     $scope.logout = function () {
         delete $window.sessionStorage.token;
         delete $rootScope.statusDateFromLink;
+        delete $rootScope.authToken;
         $sessionStorage.$reset();
         $state.go("login");
-		location.reload();
+        location.reload();
     };
 });
