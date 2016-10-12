2dd354f735e92f8d2985c2be2cbdd200fb730dab
akshay-bbytes <akshay.nag@beyondbytes.co.in>
2016-10-13 04:44:38 +0530

    cleaned import statement

--- purple-server/src/main/java/com/bbytes/purple/service/UserService.java
+++ purple-server/src/main/java/com/bbytes/purple/service/UserService.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
