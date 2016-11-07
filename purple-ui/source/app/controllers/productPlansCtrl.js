5452dcf062c9cef005c3dbf7d58b7611665e98cc
swetha-bbytes <swetha.tm@beyondbytes.co.in>
2016-11-07 11:28:21 +0530

    billing info files

---
+++ purple-ui/source/app/controllers/productPlansCtrl.js
@@ -0,0 +1,8 @@
+/*
+ * Product Plans controller
+ */
+angular.module('rootApp').controller('productPlansCtrl', function ($scope, $rootScope, userService, dropdownListService, appNotifyService) {
+
+   //write ctrl code here
+
+});
