1580c0255c9b18587f0689062c0b11e9ba515a46
tanmally <tm@beyondbytes.co.in>
2016-11-08 15:28:09 +0530

    Update dto

--- purple-server/src/main/java/com/bbytes/purple/rest/dto/models/TaskListDTO.java
+++ purple-server/src/main/java/com/bbytes/purple/rest/dto/models/TaskListDTO.java
@@ -1,24 +1,14 @@
 package com.bbytes.purple.rest.dto.models;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import com.bbytes.purple.domain.User;
 
 import lombok.Data;
-
-/**
- * @author aditya
- *
- */
 
 @Data
 public class TaskListDTO implements Serializable {
 
 	private static final long serialVersionUID = 5187445361068841595L;
-	
+
 	private String taskListId;
 
 	private String name;
